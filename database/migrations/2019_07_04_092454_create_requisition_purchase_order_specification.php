952da9752fbd9a2cc86d855dcdadbcff6dc7def4
jpcaagusan <38378503+jpcaagusan@users.noreply.github.com>
2019-07-04 14:03:33 +0800

    update table

--- database/migrations/2019_07_04_092454_create_requisition_purchase_order_specification.php
+++ database/migrations/2019_07_04_092454_create_requisition_purchase_order_specification.php
@@ -12,7 +12,7 @@ class CreateRequisitionPurchaseOrderSpecification extends Migration
      * @return void
      */
     public function up()
-    {
+    {        
         Schema::create('requisition.purchase_order_specification', function (Blueprint $table) {
             $table->bigInteger('id',true);
             $table->bigInteger('purchase_order_id');
@@ -23,6 +23,7 @@ class CreateRequisitionPurchaseOrderSpecification extends Migration
             $table->string('logs',70);
             $table->string('last_modified',70)->nullable();
             $table->timestamps();
+
             $table->foreign('purchase_order_id')
             ->references('id')
             ->on('requisition.purchase_order');
@@ -37,6 +38,6 @@ class CreateRequisitionPurchaseOrderSpecification extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('requisition.purchase_order_specification');
+        Schema::drop('requisition.purchase_order_specification');
     }
 }
