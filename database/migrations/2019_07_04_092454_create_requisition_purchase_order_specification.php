32b80c0150ea28274db7409822b338f1986226cb
jpcaagusan <38378503+jpcaagusan@users.noreply.github.com>
2019-07-04 09:51:15 +0800

    add requisition.purchase_order_specification table

---
+++ database/migrations/2019_07_04_092454_create_requisition_purchase_order_specification.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequisitionPurchaseOrderSpecification extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requisition.purchase_order_specification', function (Blueprint $table) {
+            $table->bigInteger('id',true);
+            $table->bigInteger('purchase_order_id');
+            $table->string('ponumber' , 20);
+            $table->longText('description');
+            $table->string('status',1)->default('O');
+            $table->string('logs',70);
+            $table->string('last_modified',70)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requisition.purchase_order_specification');
+    }
+}
